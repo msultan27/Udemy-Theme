d86819e3497ad2a9547086f906eae5c698a8611b
sultanshaikh27 <rizwangithub84@gmail.com>
2023-09-18 13:52:50 +0500

    Basic And Required Files

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes();  ?> >
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document </title>
+
+</head>
+<body>
+
+  <h1>language_attributes </h1>
+
+  <p>Hi rizwan shaikh how are you this is a generating Html Page.</p>
+
+  <p> Sultan Shaikh How Are You.
+  <p>If You want to successful You have to pay the price.....</p>
+
+
+</body>
+</html>
